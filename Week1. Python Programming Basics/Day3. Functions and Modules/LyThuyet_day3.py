d52ef5a45edb5d5049d73d4f926f1ca03c7aa1e8
azusak6 <tranxuanson2k61810nd@gmail.com>
2025-02-08 18:29:29 +0700

    Update LyThuyet_day3.py

--- Week1. Python Programming Basics/Day3. Functions and Modules/LyThuyet_day3.py
+++ Week1. Python Programming Basics/Day3. Functions and Modules/LyThuyet_day3.py
@@ -1,70 +1,70 @@
-# # Function - Hàm (Chương trình con) là gì
-# '''
-# + Khối mã tái hiện một nhiệm vụ cụ thể 
-# + Có thể tái sử dụng
-# + Nếu nhiều câu lệnh cùng thực hiện một hành động giống nhau thì ta có thể sử dụng function để thực hiện
-# + Hàm bắt đầu bằng từ khóa def
-# '''
+# Function - Hàm (Chương trình con) là gì
+'''
++ Khối mã tái hiện một nhiệm vụ cụ thể 
++ Có thể tái sử dụng
++ Nếu nhiều câu lệnh cùng thực hiện một hành động giống nhau thì ta có thể sử dụng function để thực hiện
++ Hàm bắt đầu bằng từ khóa def
+'''
 
-# # Syntax
-# '''
-# def function_name(paranameters):
-#     #Code block
-#     return result
-# '''
+# Syntax
+'''
+def function_name(paranameters):
+    #Code block
+    return result
+'''
 
-# # Example: Function with paranameters and return value
-# def add_numbers(a, b):
-#     return a + b
-#     # Hoặc
-#     '''
-#     c = a + b
-#     return c
-#     '''
+# Example: Function with paranameters and return value
+def add_numbers(a, b):
+    return a + b
+    # Hoặc
+    '''
+    c = a + b
+    return c
+    '''
 
-# result = add_numbers(5, 3) # gọi hàm add_number cho 5 và 3; gan 5 cho a, 3 cho b
-# print("Sum: ", result)
-# print("Sum_1: ", add_numbers(5,3))
+result = add_numbers(5, 3) # gọi hàm add_number cho 5 và 3; gan 5 cho a, 3 cho b
+print("Sum: ", result)
+print("Sum_1: ", add_numbers(5,3))
 
-# #Lưu ý: Nếu ta thay giá trị của dòng 18: 
-# '''
-# thay vì return a + b thì ta có thể thay bằng một giá trị khác nhưng phải xác định:
-#     ví dụ 1: 3 + a, 4 + 100, 5 + b, ...
-#     và khi thực hiện chuong trình ta có thể bỏ a, b trong ngoặc ở dòng 17 đi vì không quan trọng khi ta làm theo ví dụ 1 trên
-#     nghĩa là khi ta return a + 3 chẳng hạn thì ta có thể bỏ b trong dòng 17 đi và khi gọi ham 
-# '''
-
-
-
-
-# #-------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Lưu ý: Nếu ta thay giá trị của dòng 18: 
+'''
+thay vì return a + b thì ta có thể thay bằng một giá trị khác nhưng phải xác định:
+    ví dụ 1: 3 + a, 4 + 100, 5 + b, ...
+    và khi thực hiện chuong trình ta có thể bỏ a, b trong ngoặc ở dòng 17 đi vì không quan trọng khi ta làm theo ví dụ 1 trên
+    nghĩa là khi ta return a + 3 chẳng hạn thì ta có thể bỏ b trong dòng 17 đi và khi gọi ham 
+'''
 
 
 
-# # Scope and Lifetime of Variable - Phạm vi của biến trong suất chương trình
 
-# # Scope
-# '''
-# - Local Scope: Phạm vi cục bộ
-#     + Các biến được khai báo, xác định ở bên trong các hàm, các khối mã và chỉ có thể truy cập được trong khi thực hiện các hàm, khối mã chứa nó
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# - Global Scope: Phạm vi toàn cụ 
-#     + Các biến được khai báo, xác định ở bên ngoài các hàm, các khối mã và có thể truy cập được trong suất thời gian thực hiện chương trình
-# '''
 
-# # Example: 
-# a, b = 5, 6 # khai báo hai biến a, b và gán 5 cho a, 6 cho b
-# message = a + b # Global Variable
 
-# def greet():
-#     message = "Hello World!" # Local Variable
-#     print(message)
+# Scope and Lifetime of Variable - Phạm vi của biến trong suất chương trình
 
-# greet() # Gọi hàm greet()
-# print("a + b = ", message)
+# Scope
+'''
+- Local Scope: Phạm vi cục bộ
+    + Các biến được khai báo, xác định ở bên trong các hàm, các khối mã và chỉ có thể truy cập được trong khi thực hiện các hàm, khối mã chứa nó
 
-# message += 10 # Global Variable
-# print("message = ", message)
+- Global Scope: Phạm vi toàn cụ 
+    + Các biến được khai báo, xác định ở bên ngoài các hàm, các khối mã và có thể truy cập được trong suất thời gian thực hiện chương trình
+'''
+
+# Example: 
+a, b = 5, 6 # khai báo hai biến a, b và gán 5 cho a, 6 cho b
+message = a + b # Global Variable
+
+def greet():
+    message = "Hello World!" # Local Variable
+    print(message)
+
+greet() # Gọi hàm greet()
+print("a + b = ", message)
+
+message += 10 # Global Variable
+print("message = ", message)
 
 
 
@@ -128,6 +128,7 @@ def giaithua(n):
     if n == 1:
         return n
     else: return n * giaithua(n-1)
+
 
 # Trong file Test_Modules.py
 import Modules_TEST
